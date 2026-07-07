5959746be1f1e4306b040ccc83a8ba5518450561
rachit <rachitvig9@gmail.com>
2026-07-07 17:44:49 +0530

    added supporting code

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/// <reference types="google.maps" />
+import { User } from "./Users";
+
+import { Company } from "./Company";
+
+import { CustomMap } from "./CustomMap";
+const user = new User();
+const company= new Company();
+const map = new CustomMap("map");
+map.addMarker(user);
+map.addMarker(company)
